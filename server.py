13094ac1ed0ee11a3c2ed8fd4dd44f4294a7c74b
pegah <pegah.ph22@gmail.com>
2022-07-11 18:30:09 +0430

    fix server

--- server.py
+++ server.py
@@ -1,34 +1,18 @@
-from flask import Flask, Response, make_response
+from flask import Flask, request, jsonify
 import json 
-from flask_restful import Resource, Api, reqparse
+from flask_cors import CORS
 
 app = Flask(__name__)
-api = Api(app)
+CORS(app)
 invertedIndex = {'data': {}}
-    
+     
+@app.route('/', methods=["GET"])
+def getInvertedIndex():
+    return json.dumps(invertedIndex['data']), 200
 
-class InvertedIndex(Resource):
-    def get(self):
-        return json.dumps(invertedIndex['data']), 200, {"Access-Control-Allow-Origin": "*"}
-    def post(self):
-        parser = reqparse.RequestParser()  # initialize
-        parser.add_argument('invertedIndex', required=True)  # add args        
-        args = parser.parse_args()  # parse arguments to dictionary
-        invertedIndex['data'] = json.loads(args['invertedIndex'])
-
-class Search(Resource):
-    def post(self):
-        resp = make_response()
-        resp.headers.extend({"Access-Control-Allow-Origin": "*"})
-        parser = reqparse.RequestParser()  # initialize
-        parser.add_argument('query', required=True)  # add args  
-        args = parser.parse_args()  # parse arguments to dictionary
-        print(args)
-        return args, 200
-
-
-api.add_resource(InvertedIndex, '/') 
-api.add_resource(Search, '/search')  
+@app.route('/', methods=["POST"])
+def setInvertedIndex():
+    invertedIndex['data'] = request.get_json(force=True)['invertedIndex'] 
 
 if __name__ == '__main__':
     app.run()  # run our Flask app
