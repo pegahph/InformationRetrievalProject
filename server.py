e47a3b1f5a0e7c42edf9ef483b12529ccd99a672
pegah <pegah.ph22@gmail.com>
2022-07-11 17:07:16 +0430

    initialize search api

--- server.py
+++ server.py
@@ -1,11 +1,12 @@
-from flask import Flask, Response
+from flask import Flask, Response, make_response
 import json 
 from flask_restful import Resource, Api, reqparse
 
 app = Flask(__name__)
 api = Api(app)
-invertedIndex = {}
+invertedIndex = {'data': {}}
     
+
 class InvertedIndex(Resource):
     def get(self):
         return json.dumps(invertedIndex['data']), 200, {"Access-Control-Allow-Origin": "*"}
@@ -14,9 +15,20 @@ class InvertedIndex(Resource):
         parser.add_argument('invertedIndex', required=True)  # add args        
         args = parser.parse_args()  # parse arguments to dictionary
         invertedIndex['data'] = json.loads(args['invertedIndex'])
-        
-    
+
+class Search(Resource):
+    def post(self):
+        resp = make_response()
+        resp.headers.extend({"Access-Control-Allow-Origin": "*"})
+        parser = reqparse.RequestParser()  # initialize
+        parser.add_argument('query', required=True)  # add args  
+        args = parser.parse_args()  # parse arguments to dictionary
+        print(args)
+        return args, 200
+
+
 api.add_resource(InvertedIndex, '/') 
+api.add_resource(Search, '/search')  
 
 if __name__ == '__main__':
     app.run()  # run our Flask app
