7beec78a0aa24b1e16585d1cfbe8aa5957c05a4b
pegah <pegah.ph22@gmail.com>
2022-07-13 02:41:18 +0430

    save inverted index in file

--- project.py
+++ project.py
@@ -1,13 +1,14 @@
 from __future__ import unicode_literals
 from hazm import *
-import os
+import os, sys, time
 import json 
 import requests
 import webbrowser
+from os.path import exists
+import glob
+from subprocess import call
 
 BASE_PATH = os.path.abspath(os.getcwd())
-path = BASE_PATH + "/txtfiles"
-os.chdir(path)
 
 invertedIndex_dict = {}
 
@@ -38,24 +39,62 @@ def lemmatize(tokenList):
     for word in tokenList:
         lemmatizeList.append(lemmatizer.lemmatize(word))
     return lemmatizeList
+   
+def tokenizeAndCreateInvertedIndex(file):
+    normalizer = Normalizer()
+    stemmer = Stemmer()
+    normalize = normalizer.normalize(file.read())
+    stem = stemmer.stem(normalize)
+    tokenize = word_tokenize(stem)
+    lemmatizeList = lemmatize(tokenize)
+    updateInvertedIndex(omitStopWordsAndElims(lemmatizeList), docId)
 
-  
+os.chdir(BASE_PATH)
+file_exists = exists("INVERTED-INDEX.txt")
+if file_exists:
+    pass
+else:
+    path = BASE_PATH + "/txtfiles"
+    os.chdir(path)
+    for file in os.listdir():
+        docId = int(file.removesuffix(".txt"))
+        file_path =f"{path}/{file}"
+        with open(file_path, 'r', encoding="utf-8") as file:
+            normalizer = Normalizer()
+            stemmer = Stemmer()
+            normalize = normalizer.normalize(file.read())
+            stem = stemmer.stem(normalize)
+            tokenize = word_tokenize(stem)
+            lemmatizeList = lemmatize(tokenize)
+            updateInvertedIndex(omitStopWordsAndElims(lemmatizeList), docId)
 
-      
-for file in os.listdir():
-    docId = int(file.removesuffix(".txt"))
-    file_path =f"{path}/{file}"
-    with open(file_path, 'r', encoding="utf-8") as file:
-        normalizer = Normalizer()
-        stemmer = Stemmer()
-        normalize = normalizer.normalize(file.read())
-        stem = stemmer.stem(normalize)
-        tokenize = word_tokenize(stem)
-        lemmatizeList = lemmatize(tokenize)
-        updateInvertedIndex(omitStopWordsAndElims(lemmatizeList), docId)
+    os.chdir(BASE_PATH)
+    f = open("INVERTED-INDEX.txt", "x")
+    f = open("INVERTED-INDEX.txt", "w")
+    f.write(json.dumps(invertedIndex_dict))
+    f.close()
+
+    response = requests.post("http://127.0.0.1:5000", json = {'invertedIndex': json.dumps(invertedIndex_dict)})
 
 
-response = requests.post("http://127.0.0.1:5000", json = {'invertedIndex': json.dumps(invertedIndex_dict)})
+folder_path = f"{BASE_PATH}/txtfiles"
+file_type = r'/*.txt'
+files = glob.glob(folder_path + file_type)
+last_file = max(files, key=os.path.getctime)
 
 webbrowser.get('windows-default').open('file://' + BASE_PATH + '/index.html')
+
+try:
+    while True:
+        files = glob.glob(folder_path + file_type)
+        max_file = max(files, key=os.path.getctime)
+        if last_file != max_file :
+            last_file = max_file
+            print("inverted index should be update")
+        time.sleep(4)
+
+except KeyboardInterrupt:
+    pass
+
+
   
--- server.py
+++ server.py
@@ -1,14 +1,22 @@
 from flask import Flask, request, jsonify
 import json 
 from flask_cors import CORS
+import os
+from os.path import exists
 
+BASE_PATH = os.path.abspath(os.getcwd())
 app = Flask(__name__)
 CORS(app)
 invertedIndex = {'data': {}}
-     
+file_exists = exists("INVERTED-INDEX.txt")
+
+if file_exists:
+    inverted_index = open(f"{BASE_PATH}/INVERTED-INDEX.txt", "r", encoding="utf-8").read()
+    invertedIndex['data'] = inverted_index
+
 @app.route('/', methods=["GET"])
 def getInvertedIndex():
-    return json.dumps(invertedIndex['data']), 200
+    return jsonify(invertedIndex['data']), 200
 
 @app.route('/', methods=["POST"])
 def setInvertedIndex():
@@ -23,6 +31,7 @@ def searchQuery():
         return jsonify({"docs": postingList})
     except:
         return "Term not found!"
+
 
 if __name__ == '__main__':
     app.run()  # run our Flask app
